ca1a2e546d1da9dd20f2d19896cb4514ccb7217b
Mukul Mehta <mukul.csiitkgp@gmail.com>
2021-01-03 16:27:26 +0530

    Update dates for Spring 2020-2021 semester (#7)

--- dates.py
+++ dates.py
@@ -5,13 +5,13 @@ import datetime
 import pytz
 
 # SEM_BEGIN=datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-SEM_BEGIN = build_event.generateIndiaTime(2020, 9, 1, 0, 0)
+SEM_BEGIN = build_event.generateIndiaTime(2021, 1, 4, 0, 0)
 
-MID_TERM_BEGIN = build_event.generateIndiaTime(2020, 10, 23, 0, 0)
+MID_TERM_BEGIN = build_event.generateIndiaTime(2021, 2, 27, 0, 0)
 
-MID_TERM_END = build_event.generateIndiaTime(2020, 10, 27, 23, 59)
+MID_TERM_END = build_event.generateIndiaTime(2021, 3, 3, 23, 59)
 
-END_TERM_BEGIN = build_event.generateIndiaTime(2020, 11, 26, 0, 0)
+END_TERM_BEGIN = build_event.generateIndiaTime(2021, 4, 14, 0, 0)
 
 ## Sanity check
 
